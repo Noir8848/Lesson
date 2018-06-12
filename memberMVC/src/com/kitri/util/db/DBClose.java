7599ebc0776d84a323327468a13e62cbc56cd8c0
KITRI-PC\KITRI <tkddls8848@gmail.com>
2018-06-12 14:59:05 +0900

    6/12 MVC구현 1

--- memberMVC/src/com/kitri/util/db/DBClose.java
+++ memberMVC/src/com/kitri/util/db/DBClose.java
@@ -1,5 +1,88 @@
 package com.kitri.util.db;
 
+import java.sql.*;
+
 public class DBClose {
 
+	public static void close(Connection conn, Statement stmt) {
+		try {
+			if (stmt != null) {// 생성이 안될 시 발생할 null point exception회피
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			if (stmt != null) {// 생성이 안될 시 발생할 null point exception회피
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection conn, PreparedStatement ptst) {
+		try {
+			if (ptst != null) {// 생성이 안될 시 발생할 null point exception회피
+				ptst.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection conn, PreparedStatement ptst, ResultSet rs) {
+		try {
+			if (ptst != null) {// 생성이 안될 시 발생할 null point exception회피
+				ptst.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
